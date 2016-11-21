afffa48f25b0889bdf1d8a0f2812bb1bd504f5cd
McJty <mcjty1@gmail.com>
2016-11-21 10:54:54 +0100

    More fixes for mapping changes

--- src/main/java/mcjty/lib/inventory/CompatInventory.java
+++ src/main/java/mcjty/lib/inventory/CompatInventory.java
@@ -1,8 +1,16 @@
 package mcjty.lib.inventory;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 
 public interface CompatInventory extends IInventory {
+
+    boolean isUsable(EntityPlayer player);
+
+    @Override
+    default boolean isUsableByPlayer(EntityPlayer player) {
+        return isUsable(player);
+    }
 
     @Override
     default boolean isEmpty() {
--- src/main/java/mcjty/lib/inventory/CompatSidedInventory.java
+++ src/main/java/mcjty/lib/inventory/CompatSidedInventory.java
@@ -1,8 +1,16 @@
 package mcjty.lib.inventory;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ISidedInventory;
 
 public interface CompatSidedInventory extends ISidedInventory {
+
+    boolean isUsable(EntityPlayer player);
+
+    @Override
+    default boolean isUsableByPlayer(EntityPlayer player) {
+        return isUsable(player);
+    }
 
     @Override
     default boolean isEmpty() {
