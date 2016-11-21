dde27ad07aae3c11b248493a1be9eabd5f8f6a35
McJty <mcjty1@gmail.com>
2016-11-21 13:59:15 +0100

    0.0.7alpha

--- src/main/java/mcjty/lib/block/CompatItem.java
+++ src/main/java/mcjty/lib/block/CompatItem.java
@@ -5,17 +5,28 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class CompatItem extends Item {
 
     protected ActionResult<ItemStack> clOnItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
-        return new ActionResult(EnumActionResult.PASS, playerIn.getHeldItem(hand));
+        return super.onItemRightClick(worldIn, playerIn, hand);
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
         return clOnItemRightClick(worldIn, playerIn, hand);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return clOnItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    private EnumActionResult clOnItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
     }
 }
