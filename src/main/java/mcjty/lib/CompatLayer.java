16b6acbfd4c02c753d8014ed47ad0b1fd0ec4e11
McJty <mcjty1@gmail.com>
2016-11-21 14:10:17 +0100

    0.0.8alpha

--- src/main/java/mcjty/lib/CompatLayer.java
+++ src/main/java/mcjty/lib/CompatLayer.java
@@ -2,7 +2,7 @@ package mcjty.lib;
 
 public class CompatLayer {
 
-    public static final String VERSION = "1.10-0.0.7alpha";
+    public static final String VERSION = "1.10-0.0.8alpha";
     public static final String OWNER = "McJty", PROVIDES = "compatlayer";
 
 }
--- src/main/java/mcjty/lib/tools/WorldTools.java
+++ src/main/java/mcjty/lib/tools/WorldTools.java
@@ -1,11 +1,18 @@
 package mcjty.lib.tools;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class WorldTools {
 
     public static void spawnEntity(World world, Entity entity) {
         world.spawnEntity(entity);
+    }
+
+    public static void neighborChanged(World world, EnumFacing offset, Block block, BlockPos pos) {
+        world.neighborChanged(pos.offset(offset), block, pos);
     }
 }
