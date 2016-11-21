9087e9ff17e3e9dd3214150f64fbd8d8e7cfd828
McJty <mcjty1@gmail.com>
2016-11-21 16:00:31 +0100

    0.0.10alpha

--- src/main/java/mcjty/lib/block/CompatItem.java
+++ src/main/java/mcjty/lib/block/CompatItem.java
@@ -1,14 +1,14 @@
 package mcjty.lib.block;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class CompatItem extends Item {
 
@@ -28,5 +28,14 @@ public class CompatItem extends Item {
 
     protected EnumActionResult clOnItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public void getSubItems(Item itemIn, CreativeTabs tab, NonNullList<ItemStack> subItems) {
+        clGetSubItems(itemIn, tab, subItems);
+    }
+
+    protected void clGetSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+        super.getSubItems(itemIn, tab, (NonNullList<ItemStack>) subItems);
     }
 }
