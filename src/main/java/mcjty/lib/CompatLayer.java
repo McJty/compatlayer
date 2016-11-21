63a008bc93ed3b9f472a38ce709e98dea5ccece2
McJty <mcjty1@gmail.com>
2016-11-21 11:15:48 +0100

    0.0.3alpha

--- src/main/java/mcjty/lib/CompatLayer.java
+++ src/main/java/mcjty/lib/CompatLayer.java
@@ -2,7 +2,7 @@ package mcjty.lib;
 
 public class CompatLayer {
 
-    public static final String VERSION = "1.10-0.0.2alpha";
+    public static final String VERSION = "1.10-0.0.3alpha";
     public static final String OWNER = "McJty", PROVIDES = "compatlayer";
 
 }
