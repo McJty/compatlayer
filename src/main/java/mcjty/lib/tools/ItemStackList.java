5b6530311bac724dc436430cd704020f405abcfc
McJty <mcjty1@gmail.com>
2016-11-23 13:06:18 +0100

    ItemStackList fix

--- src/main/java/mcjty/lib/tools/ItemStackList.java
+++ src/main/java/mcjty/lib/tools/ItemStackList.java
@@ -2,11 +2,22 @@ package mcjty.lib.tools;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import org.apache.commons.lang3.Validate;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
 
 public class ItemStackList extends NonNullList<ItemStack> {
 
     public static ItemStackList create(int size) {
-        return (ItemStackList) ItemStackList.withSize(size, ItemStackTools.getEmptyStack());
+        Validate.notNull(ItemStackTools.getEmptyStack());
+        ItemStack[] aobject = new ItemStack[size];
+        Arrays.fill(aobject, ItemStackTools.getEmptyStack());
+        return new ItemStackList(Arrays.asList(aobject), ItemStackTools.getEmptyStack());
     }
 
+    public ItemStackList(List<ItemStack> delegateIn, @Nullable ItemStack stack) {
+        super(delegateIn, stack);
+    }
 }
