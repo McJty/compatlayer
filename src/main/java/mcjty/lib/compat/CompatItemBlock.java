98339104a4211d49e46a1c28c37c73190fa41f3c
McJty <mcjty1@gmail.com>
2016-11-24 11:23:03 +0100

    Fix

---
+++ src/main/java/mcjty/lib/compat/CompatItemBlock.java
@@ -0,0 +1,50 @@
+package mcjty.lib.compat;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class CompatItemBlock extends ItemBlock {
+
+    public CompatItemBlock(Block block) {
+        super(block);
+    }
+
+    protected ActionResult<ItemStack> clOnItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        return super.onItemRightClick(playerIn.getHeldItem(hand), worldIn, playerIn, hand);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        return clOnItemRightClick(worldIn, playerIn, hand);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return clOnItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    protected EnumActionResult clOnItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return super.onItemUse(player.getHeldItem(hand), player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public void getSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+        clGetSubItems(itemIn, tab, subItems);
+    }
+
+    protected void clGetSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+        super.getSubItems(itemIn, tab, subItems);
+    }
+}
