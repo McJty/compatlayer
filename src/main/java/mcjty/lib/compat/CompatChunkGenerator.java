f30bd65efb15b08eb4e0d37c20c8b40cb440c954
McJty <mcjty1@gmail.com>
2016-11-28 12:40:45 +0100

    0.0.23alpha

--- src/main/java/mcjty/lib/compat/CompatChunkGenerator.java
+++ src/main/java/mcjty/lib/compat/CompatChunkGenerator.java
@@ -3,6 +3,7 @@ package mcjty.lib.compat;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkGenerator;
+import net.minecraft.world.gen.structure.MapGenStructure;
 
 import javax.annotation.Nullable;
 
@@ -15,4 +16,9 @@ public interface CompatChunkGenerator extends IChunkGenerator {
     default BlockPos getStrongholdGen(World worldIn, String structureName, BlockPos position) {
         return clGetStrongholdGen(worldIn, structureName, position);
     }
+
+    public static BlockPos getClosestStrongholdPos(MapGenStructure structure, World worldIn, BlockPos pos) {
+        return structure.getClosestStrongholdPos(worldIn, pos);
+    }
+
 }
