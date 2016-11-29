07a61845fa49c8e52b6a03d7961d5430ed22de7e
McJty <mcjty1@gmail.com>
2016-11-29 11:05:46 +0100

    0.0.28alpha

--- src/main/java/mcjty/lib/tools/FluidTools.java
+++ src/main/java/mcjty/lib/tools/FluidTools.java
@@ -3,7 +3,6 @@ package mcjty.lib.tools;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
 import javax.annotation.Nonnull;
@@ -18,5 +17,17 @@ public class FluidTools {
      */
     public static ItemStack convertFluidToBucket(@Nonnull FluidStack fluidStack) {
         return FluidContainerRegistry.fillFluidContainer(fluidStack, new ItemStack(Items.BUCKET));
+    }
+
+    public static FluidStack convertBucketToFluid(@Nonnull ItemStack bucket) {
+        return FluidContainerRegistry.getFluidForFilledItem(bucket);
+    }
+
+    public static boolean isEmptyContainer(@Nonnull ItemStack itemStack) {
+        return FluidContainerRegistry.isEmptyContainer(itemStack);
+    }
+
+    public static boolean isFilledContainer(@Nonnull ItemStack itemStack) {
+        return FluidContainerRegistry.isFilledContainer(itemStack);
     }
 }
