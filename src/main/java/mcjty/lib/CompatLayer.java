3eef753700fc9910c6811dfe3c31945d52e67bc0
McJty <mcjty1@gmail.com>
2016-11-29 11:05:17 +0100

    0.0.28alpha

--- src/main/java/mcjty/lib/CompatLayer.java
+++ src/main/java/mcjty/lib/CompatLayer.java
@@ -2,7 +2,7 @@ package mcjty.lib;
 
 public class CompatLayer {
 
-    public static final String VERSION = "1.11-0.0.27alpha";
+    public static final String VERSION = "1.11-0.0.28alpha";
     public static final String OWNER = "McJty", PROVIDES = "compatlayer";
 
     public static boolean isV10() {
--- src/main/java/mcjty/lib/tools/FluidTools.java
+++ src/main/java/mcjty/lib/tools/FluidTools.java
@@ -4,7 +4,9 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+import net.minecraftforge.fluids.capability.IFluidTankProperties;
 
 import javax.annotation.Nonnull;
 
@@ -22,5 +24,64 @@ public class FluidTools {
         IFluidHandlerItem fh = FluidUtil.getFluidHandler(new ItemStack(Items.BUCKET));
         fh.fill(fluidStack, true);
         return fh.getContainer();
+    }
+
+    public static FluidStack convertBucketToFluid(@Nonnull ItemStack bucket) {
+        IFluidHandler fluidHandler = FluidUtil.getFluidHandler(bucket);
+        if (fluidHandler == null) {
+            return null;
+        }
+        IFluidTankProperties[] tankProperties = fluidHandler.getTankProperties();
+        for (IFluidTankProperties properties : tankProperties) {
+            FluidStack contents = properties.getContents();
+            if (contents != null) {
+                return contents;
+            }
+        }
+
+        return null;
+    }
+
+
+    public static boolean isEmptyContainer(@Nonnull ItemStack itemStack) {
+        IFluidHandler fluidHandler = FluidUtil.getFluidHandler(itemStack);
+        if (fluidHandler == null) {
+            return false;
+        }
+
+        IFluidTankProperties[] tankProperties = fluidHandler.getTankProperties();
+        for (IFluidTankProperties properties : tankProperties) {
+            if (properties.canFill() && properties.getCapacity() > 0) {
+                FluidStack contents = properties.getContents();
+                if (contents == null) {
+                    return true;
+                } else if (contents.amount > 0) {
+                    return false;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isFilledContainer(@Nonnull ItemStack itemStack) {
+        IFluidHandler fluidHandler = FluidUtil.getFluidHandler(itemStack);
+        if (fluidHandler == null) {
+            return false;
+        }
+
+        IFluidTankProperties[] tankProperties = fluidHandler.getTankProperties();
+        for (IFluidTankProperties properties : tankProperties) {
+            if (!properties.canDrain()) {
+                return false;
+            }
+
+            FluidStack contents = properties.getContents();
+            if (contents == null || contents.amount < properties.getCapacity()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
