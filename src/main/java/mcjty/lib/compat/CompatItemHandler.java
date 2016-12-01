75ac64392e58c94339dc4d17d0da00907d368b33
McJty <mcjty1@gmail.com>
2016-12-01 14:19:40 +0100

    0.0.38beta

---
+++ src/main/java/mcjty/lib/compat/CompatItemHandler.java
@@ -0,0 +1,8 @@
+package mcjty.lib.compat;
+
+import net.minecraftforge.items.IItemHandler;
+
+public interface CompatItemHandler extends IItemHandler {
+
+    int getSlotMaxLimit();
+}
---
+++ src/main/java/mcjty/lib/compat/CompatItemHandlerModifiable.java
@@ -0,0 +1,9 @@
+package mcjty.lib.compat;
+
+import net.minecraftforge.items.IItemHandlerModifiable;
+
+public interface CompatItemHandlerModifiable extends IItemHandlerModifiable {
+
+    int getSlotMaxLimit();
+
+}
