da2fedfcda16d8441201807e8e0cafd2470ec9d3
McJty <mcjty1@gmail.com>
2016-12-06 12:48:38 +0100

    0.1.1

---
+++ src/main/java/mcjty/lib/compat/CompatItemTool.java
@@ -0,0 +1,57 @@
+package mcjty.lib.compat;
+
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPotion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemTool;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
+import java.util.Set;
+
+public class CompatItemTool extends ItemTool {
+
+    public CompatItemTool(float attackDamageIn, float attackSpeedIn, ToolMaterial materialIn, Set<Block> effectiveBlocksIn) {
+        super(attackDamageIn, attackSpeedIn, materialIn, effectiveBlocksIn);
+    }
+
+    public CompatItemTool(ToolMaterial materialIn, Set<Block> effectiveBlocksIn) {
+        super(materialIn, effectiveBlocksIn);
+    }
+
+    protected ActionResult<ItemStack> clOnItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        return super.onItemRightClick(worldIn, playerIn, hand);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
+        return clOnItemRightClick(worldIn, playerIn, hand);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return clOnItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    protected EnumActionResult clOnItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void getSubItems(Item itemIn, CreativeTabs tab, NonNullList<ItemStack> subItems) {
+        clGetSubItems(itemIn, tab, subItems);
+    }
+
+    @SideOnly(Side.CLIENT)
+    protected void clGetSubItems(Item itemIn, CreativeTabs tab, List<ItemStack> subItems) {
+        super.getSubItems(itemIn, tab, (NonNullList<ItemStack>) subItems);
+    }
+}
