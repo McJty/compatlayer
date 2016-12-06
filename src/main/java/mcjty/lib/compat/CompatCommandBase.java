4952b4d7c6296230266ee604858cb7f04ad13b9d
McJty <mcjty1@gmail.com>
2016-12-06 12:56:13 +0100

    Command.getName() solution

--- src/main/java/mcjty/lib/compat/CompatCommandBase.java
+++ src/main/java/mcjty/lib/compat/CompatCommandBase.java
@@ -1,6 +1,7 @@
 package mcjty.lib.compat;
 
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 
 public abstract class CompatCommandBase extends CommandBase {
@@ -8,4 +9,9 @@ public abstract class CompatCommandBase extends CommandBase {
     public static boolean canUseCommand(ICommandSender sender, int permLevel, String name) {
         return sender.canUseCommand(permLevel, name);
     }
+
+    public static String getCommandName(ICommand command) {
+        return command.getName();
+    }
+
 }
