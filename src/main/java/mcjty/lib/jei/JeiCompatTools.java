844e6d984853a85e506ebb132337eee7e04a74b7
McJty <mcjty1@gmail.com>
2017-02-03 14:48:17 +0100

    0.2.1

--- src/main/java/mcjty/lib/jei/JeiCompatTools.java
+++ src/main/java/mcjty/lib/jei/JeiCompatTools.java
@@ -1,6 +1,7 @@
 package mcjty.lib.jei;
 
 import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ingredients.IIngredients;
 
 import java.util.List;
 
@@ -8,6 +9,10 @@ public class JeiCompatTools {
 
     public static void addRecipes(IModRegistry modRegistry, List recipes) {
         modRegistry.addRecipes(recipes);
+    }
+
+    public static <T> List<T> getOutputs(IIngredients ingredients, Class<T> clazz) {
+        return ingredients.getOutputs(clazz);
     }
 
 }
