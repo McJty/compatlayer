a002d2524a242d5e3c409f6a6f45105c616708bb
McJty <mcjty1@gmail.com>
2017-03-10 16:16:36 +0100

    Added CompatParticle

---
+++ src/main/java/mcjty/lib/compat/CompatParticle.java
@@ -0,0 +1,34 @@
+package mcjty.lib.compat;
+
+import net.minecraft.client.particle.Particle;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.world.World;
+
+public class CompatParticle extends Particle {
+
+    public CompatParticle(World worldIn, double posXIn, double posYIn, double posZIn) {
+        super(worldIn, posXIn, posYIn, posZIn);
+    }
+
+    public CompatParticle(World worldIn, double xCoordIn, double yCoordIn, double zCoordIn, double xSpeedIn, double ySpeedIn, double zSpeedIn) {
+        super(worldIn, xCoordIn, yCoordIn, zCoordIn, xSpeedIn, ySpeedIn, zSpeedIn);
+    }
+
+    public AxisAlignedBB clGetBoundingBox() {
+        return super.getBoundingBox();
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox() {
+        return clGetBoundingBox();
+    }
+
+    public void clSetBoundingBox(AxisAlignedBB bb) {
+        super.setBoundingBox(bb);
+    }
+
+    @Override
+    public void setBoundingBox(AxisAlignedBB bb) {
+        clSetBoundingBox(bb);
+    }
+}
