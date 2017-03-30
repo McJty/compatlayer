d48b34e65ceffd6fff17f7c7becbde94b24b33f6
McJty <mcjty1@gmail.com>
2017-03-30 14:47:03 +0200

    0.2.7

--- src/main/java/mcjty/lib/jei/JeiCompatTools.java
+++ src/main/java/mcjty/lib/jei/JeiCompatTools.java
@@ -1,7 +1,10 @@
 package mcjty.lib.jei;
 
 import mezz.jei.api.IModRegistry;
+import mezz.jei.api.gui.ICraftingGridHelper;
+import mezz.jei.api.gui.IGuiItemStackGroup;
 import mezz.jei.api.ingredients.IIngredients;
+import net.minecraft.item.ItemStack;
 
 import java.util.List;
 
@@ -13,6 +16,14 @@ public class JeiCompatTools {
 
     public static <T> List<T> getOutputs(IIngredients ingredients, Class<T> clazz) {
         return ingredients.getOutputs(clazz);
+    }
+
+    public static void setInputs(ICraftingGridHelper helper, IGuiItemStackGroup guiItemStacks, List<List<ItemStack>> input) {
+        helper.setInputStacks(guiItemStacks, input);
+    }
+
+    public static void setInputs(ICraftingGridHelper helper, IGuiItemStackGroup guiItemStacks, List<List<ItemStack>> input, int width, int height) {
+        helper.setInputStacks(guiItemStacks, input, width, height);
     }
 
 }
