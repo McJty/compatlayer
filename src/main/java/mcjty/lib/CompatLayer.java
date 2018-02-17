75f27b49ab92f3829f860a0ce992c6cce0703c73
Joseph C. Sible <josephcsible@users.noreply.github.com>
2018-02-16 23:17:46 -0500

    0.3.1

--- src/main/java/mcjty/lib/CompatLayer.java
+++ src/main/java/mcjty/lib/CompatLayer.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.Mod;
         version = CompatLayer.VERSION)
 public class CompatLayer {
 
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
     public static final String PROVIDES = "compatlayer";
 
     public static boolean isV10() {
