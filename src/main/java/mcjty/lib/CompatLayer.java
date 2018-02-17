dbc522073717a12e3649f43d25aaca3f57539516
Joseph C. Sible <josephcsible@users.noreply.github.com>
2018-02-16 22:58:31 -0500

    Give this a full mod container

--- src/main/java/mcjty/lib/CompatLayer.java
+++ src/main/java/mcjty/lib/CompatLayer.java
@@ -1,9 +1,14 @@
 package mcjty.lib;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CompatLayer.PROVIDES, name = "CompatLayer",
+        acceptedMinecraftVersions = "[1.10,1.11)",
+        version = CompatLayer.VERSION)
 public class CompatLayer {
 
     public static final String VERSION = "0.3.0";
-    public static final String OWNER = "McJty", PROVIDES = "compatlayer";
+    public static final String PROVIDES = "compatlayer";
 
     public static boolean isV10() {
         return true;
