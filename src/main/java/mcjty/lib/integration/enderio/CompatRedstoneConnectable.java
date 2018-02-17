ae48fa9dc92099fee0ee625a73a1782cb2e011f5
Joseph C. Sible <josephcsible@users.noreply.github.com>
2018-02-16 22:28:53 -0500

    Add CompatRedstoneConnectable

---
+++ src/main/java/mcjty/lib/integration/enderio/CompatRedstoneConnectable.java
@@ -0,0 +1,7 @@
+package mcjty.lib.integration.enderio;
+
+import crazypants.enderio.api.redstone.IRedstoneConnectable;
+import net.minecraftforge.fml.common.Optional;
+
+@Optional.Interface(iface = "crazypants.enderio.api.redstone.IRedstoneConnectable", modid = "EnderIO")
+public interface CompatRedstoneConnectable extends IRedstoneConnectable {}
